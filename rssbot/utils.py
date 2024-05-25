9af57c97ed29ea9350f57ff5d221d5fbd075a8bd
Bart Thate <bthate@dds.nl>
2024-05-25 13:43:31 +0200

    add attrs and utils

---
+++ rssbot/utils.py
@@ -0,0 +1,28 @@
+# This file is placed in the Public Domain.
+
+
+"utilities"
+
+
+def skip(name, skipp):
+    "check for skipping"
+    for skp in spl(skipp):
+        if skp in name:
+            return True
+    return False
+
+
+def spl(txt):
+    "split comma separated string into a list."
+    try:
+        res = txt.split(',')
+    except (TypeError, ValueError):
+        res = txt
+    return [x for x in res if x]
+
+
+def __dir__():
+    return (
+        'skip',
+        'spl'
+    )
